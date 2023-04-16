e5be8fc15e262d644fcece7e3a35cbecb525c373
valleu <2543258139@qq.com>
2023-04-16 14:28:18 +0800

    v1.0

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
+import os, re, datetime, json, logging
 from flask import Flask, request, redirect, url_for, render_template, session
 from werkzeug.security import generate_password_hash, check_password_hash
+import pymongo
 import openai
-import os
-import re
-import datetime
-import json
-import logging
-
 
 # 日志基本配置
 logging.basicConfig(filename='./logging.txt', 
@@ -14,108 +10,133 @@ logging.basicConfig(filename='./logging.txt',
                     format="%(asctime)s [%(levelname)s] %(message)s ",
                     level=logging.DEBUG # 只打印level及level以上的消息
                     )
-# 读取配置文件
-with open("./config.json", encoding='utf-8') as f:
-    config = json.load(f)
 
-# 首先从系统中获取环境变量，没有再去取配置文件中的相应变量
-PORT = os.getenv("PORT", default=config['PORT'])
-API_KEY = os.getenv("OPENAI_API_KEY", default=config['OPENAI_API_KEY'])
-openai.api_key = API_KEY
-MODEL = os.getenv("OPENAI_MODEL", default=config['OPENAI_MODEL'])
-#HTTP_PROXY = os.getenv("HTTP_PROXY", default=config['HTTP_PROXY'])
-#os.environ['HTTP_PROXY'] = HTTP_PROXY
-#HTTPS_PROXY = os.getenv("HTTPS_PROXY", default=config['HTTPS_PROXY'])
-#os.environ['HTTPS_PROXY'] = HTTPS_PROXY
-logging.debug(f'PORT = {PORT}')
-logging.debug(f'API_KEY ={API_KEY}')
-logging.debug(f'MODEL ={MODEL}')
-#logging.debug(f'HTTP_PROXY ={HTTP_PROXY}')
-#logging.debug(f'HTTPS_PROXY ={HTTPS_PROXY}')
+# 获取全局变量
+if os.getenv("DEPLOY_ON_RAILWAY"):
+    # 在railway部署：从系统中获取环境变量
+    PORT = os.getenv("PORT", default=5000)
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", default=None)
+    openai.api_key = OPENAI_API_KEY
+    OPENAI_MODEL = os.getenv("OPENAI_MODEL", default="gpt-3.5-turbo")
+    MONGO_URL = os.getenv("MONGO_URL")
+    logging.debug(f'PORT = {PORT}')
+    logging.debug(f'OPENAI_API_KEY ={OPENAI_API_KEY}')
+    logging.debug(f'OPENAI_MODEL ={OPENAI_MODEL}')
+    logging.debug(f'MONGO_URL ={MONGO_URL}')
+else:
+    # 在本地部署：读取配置文件中的变量
+    with open("./config.json", encoding='utf-8') as f:
+        config = json.load(f)  # 读取配置文件
+    PORT = config["PORT"]
+    OPENAI_API_KEY = config["OPENAI_API_KEY"]
+    openai.api_key = OPENAI_API_KEY
+    OPENAI_MODEL = config["OPENAI_MODEL"]
+    HTTP_PROXY = config['HTTP_PROXY']
+    os.environ['HTTP_PROXY'] = HTTP_PROXY
+    HTTPS_PROXY = config['HTTPS_PROXY']
+    os.environ['HTTPS_PROXY'] = HTTPS_PROXY
+    MONGO_URL = config['MONGO_URL']
+    logging.debug(f'PORT = {PORT}')
+    logging.debug(f'OPENAI_API_KEY ={OPENAI_API_KEY}')
+    logging.debug(f'OPENAI_MODEL ={OPENAI_MODEL}')
+    logging.debug(f'HTTP_PROXY ={HTTP_PROXY}')
+    logging.debug(f'HTTPS_PROXY ={HTTPS_PROXY}')
+    logging.debug(f'MONGO_URL ={MONGO_URL}')
 
-
-# 提示语集
+# 获取提示列表
 with open("./prompts.json", encoding='utf-8') as f:
     prompts = json.load(f)
 
-# 创建Flask应用
+# 创建Flask应用: 密钥用来加密session到浏览器的cookie中，保存时长为2天。
 app = Flask(__name__)
-# 密钥用来加密session到浏览器的cookie中，保存时长为7天。
 app.secret_key = os.urandom(24)
-app.permanent_session_lifetime = datetime.timedelta(days=7) 
+app.permanent_session_lifetime = datetime.timedelta(days=2) 
+
+# 获得MongoDB客户端
+mongo_client = pymongo.MongoClient(MONGO_URL)
+# 获取MongoDB的数据库和集合
+mongo_db = mongo_client['DQA']
+mongo_col = mongo_db['user_info']
 
 @app.route('/', methods=['GET', 'POST']) # 默认methods=['GET']
 def index():
-    if 'data' not in session:
+    if 'name' not in session:
+        # 1)验证阶段：没有登录
         return render_template('index.html')
-    
-    
-    if request.args.get('prompt_idx') and request.args.get('prompt_txt'):
-        # 来自/prompt的重定向
-        prompt_idx = int(request.args.get('prompt_idx'))
-        prompt_txt = request.args.get('prompt_txt')
-        if request.args.get('reset'): # 来自/prompt的重来
-            messages = []
-            session['messages'] = messages
-        else: # 来自/prompt的提交
-            messages = [{"role":"system", "index":prompt_idx, "content":prompt_txt},]
-            session['messages'] = messages
-    elif 'messages' in session and len(session['messages']) > 0:
-        # 来自/chat的重定向
-        messages = session['messages']
-        prompt_idx = messages[0]['index']
-        prompt_txt = messages[0]['content']
-    else:
-        # 来自/、/auth等
+    elif ("prompt_idx" not in session) \
+            or ("prompt" not in session) \
+            or ('messages' not in session):
+        # 2)提示阶段：没有提示
+        session['state'] = 'prompt'
         prompt_idx = 81
-        prompt_txt = prompts[prompt_idx]['prompt']
-        messages = []
-        session['messages'] = messages
-    prompt = {'index':prompt_idx, 'content':prompt_txt}
-            
-    data = session['data']
-    return render_template('index.html', prompts=prompts, prompt=prompt, messages=messages)
+        prompt = prompts[prompt_idx]['prompt']
+        session['prompt_idx'] = prompt_idx
+        session['prompt'] = prompt
+    else:
+        # 3)问答阶段：进入问答
+        session['state'] = 'chat'
+                
+    return render_template('index.html', prompts=prompts)
     
 @app.route('/auth', methods=['POST'])
 def auth():
-    user = request.form.get('user')
+    name = request.form.get('name')
     pwd = request.form.get('pwd')
     
     submit = request.form.get('submit')
     if submit == '注册':
-        if not (user and user[0].isalpha() and re.match("^[A-Za-z0-9_]*$", user)) \
+        # 验证输入
+        if not (name and name[0].isalpha() and re.match("^[A-Za-z0-9_]*$", name)) \
                 or not pwd:
             return render_template('index.html', auth_msg="输入错误")
+        if list(mongo_col.find({"name":name})) != []:
+            return render_template('index.html', auth_msg="账号存在")
+        # 用户信息
         pwd_hash = generate_password_hash(pwd)
-        data = {"user": user, "pwd_hash": pwd_hash, "messages": []}
-        filepath = "./data/{}.json".format(user) 
-        with open(filepath, 'w', encoding='utf-8') as f:
-            json.dump(data, f)
-        if not os.path.isfile(filepath):
+        user_dict = {"name": name, "pwd_hash": pwd_hash}
+        # 新增记录
+        res = mongo_col.insert_one(user_dict)
+        if list(mongo_col.find({"name":name})) == []:
             return render_template('index.html', auth_msg="注册失败")
-        logging.debug(f"signup: user = {user}")
+        logging.debug(f"注册: name={name}, pwd_hash={pwd_hash}")
         return render_template('index.html', auth_msg="注册成功")
     elif submit == '登录':
-        if not (user and user[0].isalpha() and re.match("^[A-Za-z0-9_]*$", user)) \
+        # 验证输入
+        if not (name and name[0].isalpha() and re.match("^[A-Za-z0-9_]*$", name)) \
                 or not pwd:
             return render_template('index.html', auth_msg="输入错误")
-        filepath = "./data/{}.json".format(user) 
-        if not os.path.isfile(filepath):
+        if list(mongo_col.find({"name":name})) == []:
             return render_template('index.html', auth_msg="没有注册")
-        with open(filepath, encoding='utf-8') as f:
-            data = json.load(f)
-        if not check_password_hash(data['pwd_hash'], pwd):
+        # 查询记录
+        user_dict = mongo_col.find({"name":name})[0]
+        if not check_password_hash(user_dict['pwd_hash'], pwd):
             return render_template('index.html', auth_msg="密码错误")
-        # 登录成功，存入会话。
-        session['data'] = data 
-        logging.debug(f"login: user = {user}")
+        # 写入验证会话
+        session['name'] = user_dict['name']
+        if ('prompt_idx' in user_dict) \
+                and ('prompt' in user_dict) \
+                and ('messages' in user_dict): 
+            session['prompt_idx'] = user_dict['prompt_idx']
+            session['prompt'] = user_dict['prompt']
+            session['messages'] = user_dict['messages']
+        logging.debug(f"登录: name={name}, session={session}")
         return redirect(url_for('index'))
     elif submit == '登出':
-        # 登出删除会话
-        logging.debug(f"logout: user = {user}")
-        session.pop('data', None) # 不能用del session['data']，不存在会异常。
+        # 清空所有会话：验证会话+问答会话
+        query = {"name": session["name"]}
+        if ('prompt_idx' in session) and ('prompt' in session) and ('messages' in session):
+            update = {"$set": {"prompt_idx":session["prompt_idx"], \
+                               "prompt":session["prompt"], \
+                               "messages":session["messages"]}}
+            mongo_col.update_one(query, update)    
+        session.pop('name', None) # 不能用del session['name']，不存在会异常。
+        session.pop('prompt_idx', None)
+        session.pop('prompt', None)
+        session.pop('messages', None)
+        logging.debug(f"登出: name={name}, session={session}")
         return redirect(url_for('index'))    
     else:
+        # /auth页面跳转回/页面
         return redirect(url_for('index'))
 
 @app.route('/prompt', methods=['POST'])
@@ -124,32 +145,96 @@ def prompt():
     prompt_idx = int(request.form.get('prompt_idx'))
     prompt_txt = request.form.get('prompt_txt')
     if submit == '提交':
-        return redirect(url_for('index', prompt_idx=prompt_idx, prompt_txt=prompt_txt))
+        # 写入问答会话
+        session['prompt_idx'] = prompt_idx
+        session['prompt'] = prompt_txt
+        messages = [{"role": "system", "content": prompt_txt}]
+        session['messages'] = messages
+        logging.debug("提交：prompt_idx={}, prompt={}, messages={}".format( \
+            session['prompt_idx'], session['prompt'], session['messages']))
+        return redirect(url_for('index'))
     elif submit == '重来':
+        # 清空问答会话
+        logging.debug("重来: prompt_idx={}, prompt={}, len(messages)={}".format( \
+            session.get('prompt_idx'), session.get('prompt'), len(session.get('messages'))))
+        session.pop('prompt_idx', None)
+        session.pop('prompt', None)
         session.pop('messages', None)
-        return redirect(url_for('index', prompt_idx=prompt_idx, \
-                                    prompt_txt=prompt_txt, reset=True))
+        return redirect(url_for('index'))
+    else:
+        # /auth页面跳转回/页面
+        return redirect(url_for('index'))
         
 @app.route('/chat', methods=['POST'])
 def chat():
-    chat_ask = request.form.get('chat_ask')
-    messages = session['messages']
-    messages.append({"role":"user", "content":chat_ask})
-    #completion = openai.ChatCompletion.create(model=MODEL, messages=messages) 
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-    if completion and 'choices' in completion and len(completion['choices']) > 0 \
-        and 'message' in completion['choices'][0] \
-        and 'content' in completion['choices'][0]['message']:
-        chat_answer = completion.choices[0].message.content
-    else:
-        chat_answer = ""
-    messages.append({"role":"assistant", "content":chat_answer})
-    session['messages']=messages
-    return redirect(url_for('index'))
 
+    submit = request.form.get('submit')
+    if submit == '发送':
+        # 问答服务
+        chat_ask = request.form.get('chat_ask')
+        if 'messages' not in session:
+            return redirect(url_for('index'))
+        messages = session['messages']
+        messages.append({"role":"user", "content":chat_ask})
+        chat_msg = ""
+        try:
+            #Make your OpenAI API request here
+            completion = openai.ChatCompletion.create(
+                                        model=OPENAI_MODEL, # OPENAI_MODEL == "gpt-3.5-turbo" 
+                                        messages=messages,
+                                        temperature=0.6,
+                                        max_tokens=2048) 
+            #response = openai.Completion.create(prompt="Hello world",
+            #                              model="text-davinci-003")
+        except openai.error.APIError as e:
+            #Handle API error here, e.g. retry or log
+            chat_msg = f"OpenAI API returned an API Error: {e}"
+            logging.error(chat_msg)
+            pass    # 忽略异常，继续执行。
+        except openai.error.APIConnectionError as e:
+            #Handle connection error here
+            chat_msg = f"Failed to connect to OpenAI API: {e}"
+            logging.error(chat_msg)
+            pass
+        except openai.error.RateLimitError as e:
+            #Handle rate limit error (we recommend using exponential backoff)
+            chat_msg = f"OpenAI API request exceeded rate limit: {e}"
+            logging.error(chat_msg)
+            pass
+        if chat_msg:
+            return render_template('index.html', prompts=prompts, chat_msg=chat_msg)
+        # 提取回答
+        if completion and 'choices' in completion and len(completion['choices']) > 0 \
+            and 'message' in completion['choices'][0] \
+            and 'content' in completion['choices'][0]['message']:
+            chat_answer = completion.choices[0].message.content
+        else:
+            # chat_completion_format错误
+            chat_msg = f"chat_completion_format错误： completion={completion}"
+            logging.error(chat_msg)
+            return render_template('index.html', prompts=prompts, chat_msg=chat_msg)
+        # 成功问答
+        messages.append({"role":"assistant", "content":chat_answer})
+        session['messages']=messages
+        logging.debug("回答: prompt_idx={}, prompt={}, messages={}".format( \
+                        session['prompt_idx'], session['prompt'], session['messages']))
+        return redirect(url_for('index'))
+    elif submit == '删除':
+        # message_idx为某轮问答的发问对应的messages索引
+        message_idx = request.form.get('message_idx')
+        print("type(message_idx)={}, message_idx={}".format(type(message_idx), message_idx))
+        message_idx = int(message_idx)
+        if ('messages' not in session) or (message_idx not in [1, 3, 5, 7, 9]):
+            return redirect(url_for('index'))
+        messages = session['messages']
+        old_len = len(messages)
+        # 删除message_idx和message_idx之后的所有问答
+        messages = messages[:message_idx]
+        session['messages'] = messages
+        logging.debug( \
+            "删除: prompt_idx={}, prompt={}, len(old_messages)={}, len(new_messages)={}".format( \
+            session['prompt_idx'], session['prompt'], old_len, len(session['messages'])))
+        return redirect(url_for('index'))
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=True)

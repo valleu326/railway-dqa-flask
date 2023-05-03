2e95a60403df6f31c4eb9645bf521739034b40d1
valleu <2543258139@qq.com>
2023-05-03 20:35:54 +0800

    upload files

---
+++ kqa.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pymongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+import openai
+
+class KQADatabase(object):
+    def __init__(self, mongo_url):
+        # 获得MongoDB客户端
+        self.mongo_cli = pymongo.MongoClient(mongo_url)
+        # 获取MongoDB数据库
+        self.mongo_db = self.mongo_cli['KQA']
+        # 获取用户集合
+        self.user_col = self.mongo_db['users']
+        # 获取文件集合
+        self.file_col = self.mongo_db['files']
+        
+    """
+    users集合的文档: 
+    _id域，name域, pwd_hash域，prompt域
+    """
+    def insert_user(self, name:str="", pwd:str="", \
+                    prompt:str="") -> str:
+        # 先保证名称没注册过
+        if not name or self.user_exist(name) or not pwd:
+            return None
+        # 再插入新的用户记录
+        pwd_hash = generate_password_hash(pwd)
+        doc = {'name':name, 'pwd_hash':pwd_hash, 'prompt':prompt}
+        res = self.user_col.insert_one(doc)
+        # res.inserted_id是ObjectId类型
+        # ObjectId->String: sid = str(uid)
+        # String->ObjectId: uid = ObjectId(sid)
+        uid = str(res.inserted_id)
+        return uid 
+
+    def find_user(self, name:str="", uid:str="") -> dict:
+        if not name and not uid:
+            return None
+        query = {}
+        if name:
+            query['name'] = name
+        if uid:
+            query['_id'] = ObjectId(uid)
+        result = list(self.user_col.find(query))
+        if result == []:
+            return None
+        doc = result[0]
+        doc['uid'] = str(doc['_id'])
+        return doc
+            
+    def user_exist(self, name:str="", uid:str="") -> bool:
+        return (self.find_user(name, uid) != None)
+    
+    def validate_user(self, name:str="", pwd:str="") -> bool:
+        if not name and not pwd:
+            return False    # 姓名或密码为空
+        user = self.find_user(name)
+        if not user:
+            return False    # 姓名不存在
+        return check_password_hash(user['pwd_hash'], pwd)
+
+    def update_user(self, name:str="", uid:str="", prompt:str="") -> None:
+        user = self.find_user(name, uid)
+        if not user:
+            return
+        query = {}
+        if name:
+            query['name'] = name
+        if uid:
+            query['_id'] = ObjectId(uid)
+        update = {"$set": {'prompt':prompt,}}
+        self.user_col.update_one(query, update)
+        return 
+    
+    """
+    files集合的文档: 
+    _id域，name域, title域，paragraphs域
+    """
+    def insert_file(self, name:str="", title:str="", \
+                    paragraphs:list[str]=[]) -> str:
+        if not name or not title or not paragraphs:
+            return None
+        # 已经存在就更新paragraphs
+        if self.file_exist(name, title):
+            print("insert_file: update")
+            query = {'name':name, 'title':title}
+            update = {"$set": {'paragraphs':paragraphs,}}
+            self.file_col.update_one(query, update)
+            return None
+        # 不存在才新增doc
+        print("insert_file: insert")
+        doc = {'name':name, 'title':title, \
+                       'paragraphs':paragraphs}
+        res = self.file_col.insert_one(doc)
+        uid = str(res.inserted_id)
+        return uid
+    
+    def find_files_by_user(self, name:str="") -> list:
+        if not name:
+            return []
+        query = {'name':name}
+        results = list(self.file_col.find(query))
+        # result可能为[]
+        return results
+
+    def find_file(self, name:str="", title:str="") -> dict:
+        if not name or not title:
+            return None
+        query = {'name':name, 'title':title}
+        result = list(self.file_col.find(query))
+        if result == []:
+            return None
+        doc = result[0]
+        return doc
+    
+    def file_exist(self, name:str="", title:str="") -> bool:
+        return (self.find_file(name, title) != None)
+
+    def delete_file(self, name:str="", title:str=""):
+        if not name or not title:
+            return
+        query = {'name':name, 'title':title}
+        self.file_col.delete_one(query)
+        return
+
+class KQALangModel(object):
+    def __init__(self, openai_api_key, openai_model):
+        # 设置openai的api key
+        openai.api_key = openai_api_key
+        # openai_model是"gpt-3.5-turbo"或"gpt-4"
+        self.openai_model = openai_model
+    
+    def answer(self, messages):
+        err_msg = ""
+        try:
+            #Make your OpenAI API request here
+            completion = openai.ChatCompletion.create(
+                                        model=self.openai_model,
+                                        messages=messages,
+                                        temperature=0.6,
+                                        max_tokens=2048) 
+        except openai.error.APIError as e:
+            #Handle API error here, e.g. retry or log
+            err_msg = "OpenAI API调用出错"
+            print(f"OpenAI API returned an API Error: {e}")
+            pass    # 忽略异常，继续执行。
+        except openai.error.APIConnectionError as e:
+            #Handle connection error here
+            err_msg = "OpenAI API连接失败"
+            print(f"Failed to connect to OpenAI API: {e}")
+            pass
+        except openai.error.RateLimitError as e:
+            #Handle rate limit error (we recommend using exponential backoff)
+            err_msg = "OpenAI API频繁访问"
+            print(f"OpenAI API request exceeded rate limit: {e}")
+            pass
+        if err_msg != "":
+            return (False, err_msg)
+        if not(completion and 'choices' in completion \
+            and len(completion['choices']) > 0 \
+            and 'message' in completion['choices'][0] \
+            and 'content' in completion['choices'][0]['message']):
+            err_msg = "OpenAI API格式错误"
+            print(f"OpenAI API return format error: completion={completion}")
+            return (False, err_msg)
+        chat_answer = completion.choices[0].message.content
+        return (True, chat_answer)
--- main.py
+++ main.py
@@ -1,143 +1,223 @@
-import os, re, datetime, json, logging
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os, re, datetime, json
 from flask import Flask, request, redirect, url_for, render_template, session
-from werkzeug.security import generate_password_hash, check_password_hash
-import pymongo
-import openai
-
-# 日志基本配置
-logging.basicConfig(filename='./logging.txt', 
-                    filemode='a',       # 'w'为覆盖，'a'为追加
-                    format="%(asctime)s [%(levelname)s] %(message)s ",
-                    level=logging.DEBUG # 只打印level及level以上的消息
-                    )
+from unstructured.partition.doc import partition_doc
+from unstructured.partition.docx import partition_docx
+import requests
+from bs4 import BeautifulSoup
+from kqa import KQALangModel, KQADatabase
 
 # 获取全局变量
 if os.getenv("DEPLOY_ON_RAILWAY"):
     # 在railway部署：从系统中获取环境变量
     PORT = os.getenv("PORT")
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    openai.api_key = OPENAI_API_KEY
     OPENAI_MODEL = os.getenv("OPENAI_MODEL")
     MONGO_URL = os.getenv("MONGO_URL")
-    logging.debug(f'PORT = {PORT}')
-    logging.debug(f'OPENAI_API_KEY ={OPENAI_API_KEY}')
-    logging.debug(f'OPENAI_MODEL ={OPENAI_MODEL}')
-    logging.debug(f'MONGO_URL ={MONGO_URL}')
 else:
     # 在本地部署：读取配置文件中的变量
-    with open("./config.json", encoding='utf-8') as f:
-        config = json.load(f)  # 读取配置文件
+    with open("./config.json", encoding='utf-8') as config_fid:
+        config = json.load(config_fid)  # 读取配置文件
     PORT = config["PORT"]
     OPENAI_API_KEY = config["OPENAI_API_KEY"]
-    openai.api_key = OPENAI_API_KEY
     OPENAI_MODEL = config["OPENAI_MODEL"]
-    HTTP_PROXY = config['HTTP_PROXY']
-    os.environ['HTTP_PROXY'] = HTTP_PROXY
-    HTTPS_PROXY = config['HTTPS_PROXY']
-    os.environ['HTTPS_PROXY'] = HTTPS_PROXY
     MONGO_URL = config['MONGO_URL']
-    logging.debug(f'PORT = {PORT}')
-    logging.debug(f'OPENAI_API_KEY ={OPENAI_API_KEY}')
-    logging.debug(f'OPENAI_MODEL ={OPENAI_MODEL}')
-    logging.debug(f'HTTP_PROXY ={HTTP_PROXY}')
-    logging.debug(f'HTTPS_PROXY ={HTTPS_PROXY}')
-    logging.debug(f'MONGO_URL ={MONGO_URL}')
+    os.environ['HTTP_PROXY'] = config['HTTP_PROXY']
+    os.environ['HTTPS_PROXY'] = config['HTTPS_PROXY']
+print("================")
+print(f'PORT={PORT}')
+print(f'OPENAI_API_KEY={OPENAI_API_KEY}')
+print(f'OPENAI_MODEL={OPENAI_MODEL}')
+print(f'MONGO_URL={MONGO_URL}')
+print("================")
 
-
-# 获取提示列表 
-with open("./prompts.json", encoding='utf-8') as f:
-    prompts = json.load(f)
-
-# 创建Flask应用: 密钥用来加密session到浏览器的cookie中，保存时长为2天。
+# 创建Flask应用
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
-app.permanent_session_lifetime = datetime.timedelta(days=2) 
+# 密钥用来加密session到浏览器的cookie中
+# 等价于 app.secret_key = "who dares win"
+app.config['SECRET_KEY'] = "who dares win"
+# session保存时长为1天
+# 等价于app.permanent_session_lifetime = datetime.timedelta(days=1) 
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=1)
+# 限制上传文件不超过16MB
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  
 
-# 获得MongoDB客户端
-mongo_client = pymongo.MongoClient(MONGO_URL)
-# 获取MongoDB的数据库和集合
-mongo_db = mongo_client['DQA']
-mongo_col = mongo_db['user_info']
+# 创建语言模型
+lang_model = KQALangModel(OPENAI_API_KEY, OPENAI_MODEL)
 
-@app.route('/', methods=['GET', 'POST']) # 默认methods=['GET']
+# 创建数据库
+db = KQADatabase(MONGO_URL)
+
+@app.route('/') # 默认methods=['GET']
 def index():
-    if 'name' not in session:
-        # 1)验证阶段：没有登录
-        return render_template('index.html')
-    elif ("prompt_idx" not in session) \
-            or ("prompt" not in session) \
-            or ('messages' not in session):
-        # 2)提示阶段：没有提示
-        session['state'] = 'prompt'
-        prompt_idx = 81
-        prompt = prompts[prompt_idx]['prompt']
-        session['prompt_idx'] = prompt_idx
-        session['prompt'] = prompt
-    else:
-        # 3)问答阶段：进入问答
-        session['state'] = 'chat'
-                
-    return render_template('index.html', prompts=prompts)
-    
-@app.route('/auth', methods=['POST'])
-def auth():
+    # state in ['register', 'login', 'prompt', 'chat']
+    # 登录阶段：没有登录，要先登录。
+    state = 'login'
+    if ('name' in session) and ('uid' in session) and \
+        db.user_exist(name=session['name'], uid=session['uid']):
+        # 提示阶段：已经登录，没有提示。
+        state = 'prompt'
+        if ("prompt" in session) and ("messages" in session):
+            # 交互阶段：已有提示，进入问答。
+            state = 'chat'  
+    print(f"主页: state={state}")
+    return render_template('index.html', state=state)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('index.html', state='register')
+        
+    assert(request.method == 'POST')
+    # 用户信息
     name = request.form.get('name')
     pwd = request.form.get('pwd')
-    
+    pwd2 = request.form.get('pwd2')
+    # 验证输入
+    if (not pwd) or (not pwd2) or (pwd != pwd2):
+        return render_template('index.html', \
+                    state='register', auth_msg="密码错误")
+    if (not name) or (not re.match("^[A-Za-z]+[A-Za-z0-9_]*$", name)):
+        return render_template('index.html', \
+                    state='register', auth_msg="名称错误")       
+    # 新增用户
+    uid = db.insert_user(name=name, pwd=pwd) 
+    if not uid:
+        return render_template('index.html', \
+                    state='register', auth_msg="账号存在") 
+    print(f"注册: name={name}, uid={uid}")
+    return render_template('index.html', \
+                    state='login', auth_msg="注册成功")
+
+@app.route('/login', methods=['POST'])
+def login():
+    # 用户信息
+    name = request.form.get('name')
+    pwd = request.form.get('pwd')
+    # 验证输入
+    if (not pwd) or (not name) \
+            or (not re.match("^[A-Za-z]+[A-Za-z0-9_]*$", name)):
+        return render_template('index.html', \
+                    state='login', auth_msg="输入错误")
+    # 查询记录
+    if not db.validate_user(name, pwd):
+        return render_template('index.html', \
+                    state='login', auth_msg="登录失败")
+    # 写入会话：相当于登录成功。
+    user = db.find_user(name)
+    session['name'] = user['name']
+    session['uid'] = user['uid']
+    if 'prompt' in user and user['prompt'] != "":
+        session['prompt'] = user['prompt']
+        messages = [{"role": "system", "content": user['prompt']}]
+        session['messages'] = messages
+    titles = [f['title'] for f in db.find_files_by_user(name)]
+    session['titles'] = titles
+    print(f"登录: name={name}, uid={session['uid']}")
+    return redirect(url_for('index'))
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    name = session.get('name')
+    uid = session.get('uid')
+    if name and uid:
+        print(f"登出: name={name}, uid={uid}")
+    # 清空会话：相当于登出成功。
+    ## 不能用del session['xxx']，不存在时会异常。
+    #session.pop('name', None) 
+    #session.pop('uid', None)
+    #session.pop('prompt', None)
+    #session.pop('messages', None)
+    session.clear()
+    return redirect(url_for('index'))    
+
+@app.route('/upload', methods=['POST'])
+def upload():
     submit = request.form.get('submit')
-    if submit == '注册':
-        # 验证输入
-        if not (name and name[0].isalpha() and re.match("^[A-Za-z0-9_]*$", name)) \
-                or not pwd:
-            return render_template('index.html', auth_msg="输入错误")
-        if list(mongo_col.find({"name":name})) != []:
-            return render_template('index.html', auth_msg="账号存在")
-        # 用户信息
-        pwd_hash = generate_password_hash(pwd)
-        user_dict = {"name": name, "pwd_hash": pwd_hash}
-        # 新增记录
-        res = mongo_col.insert_one(user_dict)
-        if list(mongo_col.find({"name":name})) == []:
-            return render_template('index.html', auth_msg="注册失败")
-        logging.debug(f"注册: name={name}, pwd_hash={pwd_hash}")
-        return render_template('index.html', auth_msg="注册成功")
-    elif submit == '登录':
-        # 验证输入
-        if not (name and name[0].isalpha() and re.match("^[A-Za-z0-9_]*$", name)) \
-                or not pwd:
-            return render_template('index.html', auth_msg="输入错误")
-        if list(mongo_col.find({"name":name})) == []:
-            return render_template('index.html', auth_msg="没有注册")
-        # 查询记录
-        user_dict = mongo_col.find({"name":name})[0]
-        if not check_password_hash(user_dict['pwd_hash'], pwd):
-            return render_template('index.html', auth_msg="密码错误")
-        # 写入验证会话
-        session['name'] = user_dict['name']
-        if ('prompt_idx' in user_dict) \
-                and ('prompt' in user_dict) \
-                and ('messages' in user_dict): 
-            session['prompt_idx'] = user_dict['prompt_idx']
-            session['prompt'] = user_dict['prompt']
-            session['messages'] = user_dict['messages']
-        logging.debug(f"登录: name={name}, session={session}")
+    if submit == '上传':
+        if 'file' not in request.files:
+            return redirect(url_for('index'))
+        # 从文件中提取title和paragraphs
+        file = request.files['file']
+        filename, filetype = os.path.splitext(file.filename)
+        title = filename.strip()
+        filetype = filetype.lower()
+        if filetype not in ['.txt', '.doc', '.docx']:
+            # 永不进入：form要求是3种文件类型中的一种。
+            return redirect(url_for('index')) 
+        filepath = "./tmp" # 保存到临时文件中
+        file.save(filepath)
+        if filetype == '.txt':
+            with open(filepath, "r") as f:
+                paragraphs = f.readlines()
+        elif filetype == '.doc':
+            paragraphs = partition_doc(filename=filepath)
+        elif filetype == '.docx':
+            paragraphs = partition_docx(filename=filepath)
+        paragraphs = [str(p).strip() for p in paragraphs \
+                                      if str(p).strip() != ""]
+        # 新增文件
+        uid = db.insert_file(name=session['name'], title=title, \
+                                       paragraphs=paragraphs)
+        if uid != None:
+            titles = session['titles']
+            titles.append(title)
+            session['titles'] = titles
+        print(f"上传: title={title}")
+        # print("paragraphs:")
+        # for i, p in enumerate(paragraphs):
+        #     print(f"[{i}]{p}")
         return redirect(url_for('index'))
-    elif submit == '登出':
-        # 清空所有会话：验证会话+问答会话
-        query = {"name": session["name"]}
-        if ('prompt_idx' in session) and ('prompt' in session) and ('messages' in session):
-            update = {"$set": {"prompt_idx":session["prompt_idx"], \
-                               "prompt":session["prompt"], \
-                               "messages":session["messages"]}}
-            mongo_col.update_one(query, update)    
-        session.pop('name', None) # 不能用del session['name']，不存在会异常。
-        session.pop('prompt_idx', None)
-        session.pop('prompt', None)
-        session.pop('messages', None)
-        logging.debug(f"登出: name={name}, session={session}")
-        return redirect(url_for('index'))    
+    elif submit == '抓取':    
+        # 下载网页
+        url = request.form.get('url')
+        response = requests.get(url=url)
+        # 从网页中提取title和paragraphs
+        charset = requests.utils.get_encodings_from_content(response.text)[0]
+        soup = BeautifulSoup( \
+            response.text.encode(response.encoding).decode(charset), \
+            'html.parser', from_encoding=charset)
+        result = soup.find('h1')
+        if result:
+            title = result.text
+        else:
+            result = soup.find('h2')
+            if result:
+                title = result.text
+            else:
+                title = soup.find('title').text
+        title = title.strip()
+        paragraphs = []
+        for p in soup.find_all('p'):
+            if p.text.strip() != "":
+                paragraphs.append(p.text.strip())
+        # 新增文件
+        uid = db.insert_file(name=session['name'], title=title, \
+                                       paragraphs=paragraphs)
+        if uid != None:
+            titles = session['titles']
+            titles.append(title)
+            session['titles'] = titles
+        print(f"抓取: title={title}")
+        # print("paragraphs:")
+        # for i, p in enumerate(paragraphs):
+        #     print("f[{i}]{p}")
+        return redirect(url_for('index'))
+    elif submit == '删除': 
+        title_idx = request.form.get('title_idx')
+        title_idx = int(title_idx)
+        if 'titles' not in session or 'name' not in session:
+            return redirect(url_for('index'))
+        # 删除文件
+        title = session['titles'][title_idx]
+        titles = session['titles']
+        titles.pop(title_idx)
+        session['titles'] = titles
+        db.delete_file(name=session['name'], title=title)
+        print(f"删文: title={title}")
+        return redirect(url_for('index'))
     else:
-        # /auth页面跳转回/页面
         return redirect(url_for('index'))
 
 @app.route('/prompt', methods=['POST'])
@@ -145,34 +225,30 @@ def prompt():
     submit = request.form.get('submit')
     if submit == '提交':
         # 写入问答会话
-        prompt_idx = int(request.form.get('prompt_idx'))
-        prompt_txt = request.form.get('prompt_txt')
-        session['prompt_idx'] = prompt_idx
-        session['prompt'] = prompt_txt
-        messages = [{"role": "system", "content": prompt_txt}]
+        prompt = request.form.get('prompt')
+        if prompt == "":
+            return render_template('index.html', \
+                        state='prompt', prompt_msg="提示不能为空")
+        messages = [{"role": "system", "content": prompt}]
+        session['prompt'] = prompt
         session['messages'] = messages
-        mongo_col.update_one({"name": session["name"]}, \
-                {"$set": {"prompt_idx":session["prompt_idx"], \
-                          "prompt":session["prompt"], \
-                          "messages":session["messages"]}}) 
-        logging.debug("提交：prompt_idx={}, prompt={}, messages={}".format( \
-            session['prompt_idx'], session['prompt'], session['messages']))
+        # 保存prompt到数据库中
+        db.update_user(name=session['name'], uid=session['uid'], \
+                       prompt=session["prompt"])
+        print(f"提示：prompt={prompt}, messages={messages}")
         return redirect(url_for('index'))
     elif submit == '重来':
         # 清空问答会话
-        logging.debug("重来: prompt_idx={}, prompt={}, len(messages)={}".format( \
-            session.get('prompt_idx'), session.get('prompt'), len(session.get('messages'))))
-        session.pop('prompt_idx', None)
+        print(f"重来: prompt={session.get('prompt')}")
         session.pop('prompt', None)
         session.pop('messages', None)
+        # 清除数据库中的prompt
+        db.update_user(name=session['name'], uid=session['uid'], prompt="")
         return redirect(url_for('index'))
-    else:
-        # /auth页面跳转回/页面
-        return redirect(url_for('index'))
+    return redirect(url_for('index'))
         
 @app.route('/chat', methods=['POST'])
 def chat():
-
     submit = request.form.get('submit')
     if submit == '发送':
         # 问答服务
@@ -181,83 +257,36 @@ def chat():
             return redirect(url_for('index'))
         messages = session['messages']
         messages.append({"role":"user", "content":chat_ask})
-        chat_msg = ""
-        try:
-            #Make your OpenAI API request here
-            completion = openai.ChatCompletion.create(
-                                        model=OPENAI_MODEL, # OPENAI_MODEL == "gpt-3.5-turbo" 
-                                        messages=messages,
-                                        temperature=0.6,
-                                        max_tokens=2048) 
-            #response = openai.Completion.create(prompt="Hello world",
-            #                              model="text-davinci-003")
-        except openai.error.APIError as e:
-            #Handle API error here, e.g. retry or log
-            chat_msg = f"OpenAI API returned an API Error: {e}"
-            logging.error(chat_msg)
-            pass    # 忽略异常，继续执行。
-        except openai.error.APIConnectionError as e:
-            #Handle connection error here
-            chat_msg = f"Failed to connect to OpenAI API: {e}"
-            logging.error(chat_msg)
-            pass
-        except openai.error.RateLimitError as e:
-            #Handle rate limit error (we recommend using exponential backoff)
-            chat_msg = f"OpenAI API request exceeded rate limit: {e}"
-            logging.error(chat_msg)
-            pass
-        if chat_msg:
-            return render_template('index.html', prompts=prompts, chat_msg=chat_msg)
-        # 提取回答
-        if completion and 'choices' in completion and len(completion['choices']) > 0 \
-            and 'message' in completion['choices'][0] \
-            and 'content' in completion['choices'][0]['message']:
-            chat_answer = completion.choices[0].message.content
-        else:
-            # chat_completion_format错误
-            chat_msg = f"chat_completion_format错误： completion={completion}"
-            logging.error(chat_msg)
-            return render_template('index.html', prompts=prompts, chat_msg=chat_msg)
-        # 成功问答
+        is_ok, result = lang_model.answer(messages)
+        # 回答失败
+        if not is_ok:
+            err_msg = result
+            return render_template('index.html', \
+                                   state='chat', chat_msg=err_msg)
+        # 回答成功
+        chat_answer = result
         messages.append({"role":"assistant", "content":chat_answer})
         session['messages']=messages
-        mongo_col.update_one({"name": session["name"]}, \
-                {"$set": {"messages":session["messages"]}})
-        logging.debug("回答: prompt_idx={}, prompt={}, messages={}".format( \
-                        session['prompt_idx'], session['prompt'], session['messages']))
+        print(f"问答: chat_ask={chat_ask}, chat_answer={chat_answer}")
         return redirect(url_for('index'))
     elif submit == '删除':
         # message_idx为某轮问答的发问对应的messages索引
         message_idx = request.form.get('message_idx')
         message_idx = int(message_idx)
-        if ('messages' not in session) or (message_idx not in [1, 3, 5, 7, 9]):
+        # message_idx对应发问的下标，应该是奇数。
+        if ('messages' not in session) or (message_idx % 2 == 0):
             return redirect(url_for('index'))
         messages = session['messages']
         old_len = len(messages)
         # 删除message_idx和message_idx之后的所有问答
         messages = messages[:message_idx]
         session['messages'] = messages
-        mongo_col.update_one({"name": session["name"]}, \
-                {"$set": {"messages":session["messages"]}})
-        logging.debug( \
-            "删除: prompt_idx={}, prompt={}, len(old_messages)={}, len(new_messages)={}".format( \
-            session['prompt_idx'], session['prompt'], old_len, len(session['messages'])))
+        new_len = len(messages)
+        print(f"删除: len(old_messages)={old_len}, len(new_messages)={new_len}")
         return redirect(url_for('index'))
-    else:
-        return redirect(url_for('index'))
+    return redirect(url_for('index'))
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=True)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
